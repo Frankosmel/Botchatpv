84df83f1f8124dd6459cf0a408cc08b10c5b59c0
Frankosmel <130179322+Frankosmel@users.noreply.github.com>
2025-06-25 22:09:43 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-from utils.json_utils import read_json
+from json_utils import read_json
 
-CONFIG = read_json("data/config.json")
+CONFIG = read_json("config.json")
 
 BOT_TOKEN = CONFIG["bot_token"]
 ADMINS = CONFIG["admins"]
